a5da8e337561f226bfae1ea563bdddab8d62400d
Llorente Castelblanque <emiliano-javier.llorente-castelblanque@capgemini.com>
2023-03-23 10:00:07 +0100

    Creada interfaz JuegoDao

---
+++ src/main/java/org/capgemini/proyecto1/repository/JuegoDao.java
@@ -0,0 +1,8 @@
+package org.capgemini.proyecto1.repository;
+
+import org.capgemini.proyecto1.model.Juego;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JuegoDao extends JpaRepository<Juego, Integer>{
+
+}
