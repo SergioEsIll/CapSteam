f63a9aac96580dd78effd29600c858e9fb8c8936
Espejo Illanes <sergio.espejo-illanes@capgemini.com>
2023-03-23 10:58:32 +0100

    Creado metodo listJuego en el controlador

--- src/main/java/org/capgemini/proyecto1/controller/JuegoController.java
+++ src/main/java/org/capgemini/proyecto1/controller/JuegoController.java
@@ -1,8 +1,21 @@
 package org.capgemini.proyecto1.controller;
 
+import org.capgemini.proyecto1.service.JuegoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JuegoController {
+	
+	@Autowired
+	JuegoService juegoService;
+	
+	@GetMapping("/")
+	public String listJuego(Model model) {
+		model.addAttribute("listadoJuego", juegoService.findAll());
+		return "listadoJuego.html";
+	}
 
 }
