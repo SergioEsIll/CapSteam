7ac4b08acd73cbc4fad6814223f002c4de4e22b2
Llorente Castelblanque <emiliano-javier.llorente-castelblanque@capgemini.com>
2023-03-23 10:04:08 +0100

    Añadido @Repository

--- src/main/java/org/capgemini/proyecto1/repository/JuegoDao.java
+++ src/main/java/org/capgemini/proyecto1/repository/JuegoDao.java
@@ -2,7 +2,9 @@ package org.capgemini.proyecto1.repository;
 
 import org.capgemini.proyecto1.model.Juego;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JuegoDao extends JpaRepository<Juego, Integer>{
 
 }
