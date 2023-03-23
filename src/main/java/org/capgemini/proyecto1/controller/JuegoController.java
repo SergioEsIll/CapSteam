94fdd318864f5a36046ede84bd8a72bd334843e7
Mesa Núñez <ayoze.mesa-nunez@capgemini.com>
2023-03-23 23:21:47 +0100

    Creado el método uploadFile en el controlador y la clase ResponseMessage

--- src/main/java/org/capgemini/proyecto1/controller/JuegoController.java
+++ src/main/java/org/capgemini/proyecto1/controller/JuegoController.java
@@ -1,19 +1,48 @@
 package org.capgemini.proyecto1.controller;
 
+import org.capgemini.proyecto1.helper.CSVHelper;
+import org.capgemini.proyecto1.message.ResponseMessage;
 import org.capgemini.proyecto1.model.Juego;
+import org.capgemini.proyecto1.service.CSVService;
 import org.capgemini.proyecto1.service.JuegoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class JuegoController {
 
 	@Autowired
 	JuegoService juegoService;
+	
+	@Autowired
+	CSVService fileService;
+
+	@PostMapping("/upload")
+	public ResponseEntity<ResponseMessage> uploadFile(@RequestParam("file") MultipartFile file) {
+		String message = "";
+
+		if (CSVHelper.hasCSVFormat(file)) {
+			try {
+				fileService.save(file);
+
+				message = "Uploaded the file successfully: " + file.getOriginalFilename();
+				return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
+			} catch (Exception e) {
+				message = "Could not upload the file: " + file.getOriginalFilename() + "!";
+				return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).body(new ResponseMessage(message));
+			}
+		}
+
+		message = "Please upload a csv file!";
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(message));
+	}
 
 	/**
 	 * 
