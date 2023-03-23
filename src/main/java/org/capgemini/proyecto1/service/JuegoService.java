0bcc11f8f78d6611aea974e82a6da3b2cc1b9564
SergioEsIll <128601504+SergioEsIll@users.noreply.github.com>
2023-03-23 11:11:05 +0100

    Revert "Cp52"

--- src/main/java/org/capgemini/proyecto1/service/JuegoService.java
+++ src/main/java/org/capgemini/proyecto1/service/JuegoService.java
@@ -4,13 +4,8 @@ import java.util.List;
 
 import org.capgemini.proyecto1.model.Juego;
 
-import com.ejemplos.spring.model.User;
-
 public interface JuegoService {
 	
 	public List<Juego> findAll();
-	
-	// Para Update & Add
-	public void saveJuego(Juego juego);
 
 }
