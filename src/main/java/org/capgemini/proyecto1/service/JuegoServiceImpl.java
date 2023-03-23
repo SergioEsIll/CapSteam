7adf9c81b85d3bcd296591532de1c0f101dc4eb1
Espejo Illanes <sergio.espejo-illanes@capgemini.com>
2023-03-23 10:46:57 +0100

    Creado JuegoServiceImpl con metodos de interfaz

---
+++ src/main/java/org/capgemini/proyecto1/service/JuegoServiceImpl.java
@@ -0,0 +1,21 @@
+package org.capgemini.proyecto1.service;
+
+import java.util.List;
+
+import org.capgemini.proyecto1.model.Juego;
+import org.capgemini.proyecto1.repository.JuegoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JuegoServiceImpl implements JuegoService {
+	
+	@Autowired
+	JuegoDao juegoDao;
+
+	@Override
+	public List<Juego> findAll() {
+		return juegoDao.findAll();
+	}
+
+}
