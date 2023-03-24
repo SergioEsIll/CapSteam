9b039127abf7d1d4e5f7a23f5ac6143f2026c7de
Llorente Castelblanque <emiliano-javier.llorente-castelblanque@capgemini.com>
2023-03-24 10:07:17 +0100

    Crear Query Select DISTINCT platform from juegos en el respositorio DAO

--- src/main/java/org/capgemini/proyecto1/repository/JuegoDao.java
+++ src/main/java/org/capgemini/proyecto1/repository/JuegoDao.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.capgemini.proyecto1.model.Juego;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,5 +13,8 @@ public interface JuegoDao extends JpaRepository<Juego, Integer>{
 	public List<Juego> findByPlatform(String platform);
 
 	public List<Juego> findByGenre(String genre);
+	
+	@Query("SELECT DISTINCT j.platform FROM Juego j")
+	public String[] findAllPlatforms();
 	
 }
